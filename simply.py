b2e52f49795c98fd164fa9715bf0c490a95637ff
Myname <muthuswamynadar25@gmail.com>
2024-09-01 18:56:23 +0530

    added it

--- simply.py
+++ simply.py
@@ -1,10 +1,19 @@
+import time
+
 import aiml
 
 kernel = aiml.Kernel()
 kernel.learn("std-startup.xml")
 kernel.respond("load aiml b")
-
+# try:
+#     while True:
+#         input_text = input(">Human: ")
+#         response = kernel.respond(input_text)
+#         print(">Bot: " + response)
+# except KeyboardInterrupt:
+#     print('Interrupted by user')
 while True:
     input_text = input(">Human: ")
     response = kernel.respond(input_text)
     print(">Bot: "+response)
+    time.sleep(0.5)
